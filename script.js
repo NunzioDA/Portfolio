0daee62583ccef8c3801838dd3c5292c0c2286b1
NunzioDA <nunziodamore@gmail.com>
2025-04-01 00:23:10 +0200

    added style for topbar buttons

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const sections = document.querySelectorAll('.section');
+const topbarbuttons = document.querySelectorAll('.topbar-button');
+
+
 let activeSection = 0;
+
+function scrollToSection(id) {
+    const section = document.getElementById(id);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
 
 function showSection(index) {
     sections.forEach((section, i) => {
         if (i === index) {
             section.classList.add('active');
+            topbarbuttons[i].classList.add('active');
         } else {
+            topbarbuttons[i].classList.remove('active');
             section.classList.remove('active');
         }
     });
